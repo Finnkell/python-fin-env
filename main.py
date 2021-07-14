e5ded66a5610ed07909a2505fc317d1ddcdf4da5
Eric Batista <klose.eric31@gmail.com>
2021-07-14 16:37:52 -0300

    DONE: create MFI setup

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# %%
 import threading
 import concurrent.futures
 import subprocess
@@ -5,6 +6,7 @@ import time
 import random
 from datetime import datetime
 from src.models.svm import SVRModel, NuSVRModel, LinearSVRModel, SVCModel, NuSVCModel, LinearSVCModel
+from src.setups.mfi import MFISetup
 
 
 model_1 = SVRModel()
@@ -16,27 +18,33 @@ model_6 = LinearSVCModel()
 
 start = time.perf_counter()
 
-t1 = threading.Thread(target=model_1.example_model_boston)
-t2 = threading.Thread(target=model_2.example_model_diabetes)
-t3 = threading.Thread(target=model_3.example_model_diabetes)
-t4 = threading.Thread(target=model_4.example_model_rcv1)
-t5 = threading.Thread(target=model_5.example_model_rcv1)
-t6 = threading.Thread(target=model_6.example_model_rcv1)
+# t1 = threading.Thread(target=model_1.example_model_diabetes)
+# t2 = threading.Thread(target=model_2.example_model_diabetes)
+# t3 = threading.Thread(target=model_3.example_model_diabetes)
+# t4 = threading.Thread(target=model_4.example_model_breast_cancer)
+# t5 = threading.Thread(target=model_5.example_model_breast_cancer)
+# t6 = threading.Thread(target=model_6.example_model_breast_cancer)
 
 
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
+# t1.start()
+# t2.start()
+# t3.start()
+# t4.start()
+# t5.start()
+# t6.start()
 
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
-t6.join()
+# t1.join()
+# t2.join()
+# t3.join()
+# t4.join()
+# t5.join()
+# t6.join()
+
+setup = MFISetup()
+
+setup.example()
+
+setup.calculations()
 
 finish = time.perf_counter()
 
