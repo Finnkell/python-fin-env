f6ad7719d88a581c0ca32f1fd9a2919b6af929e0
Eric Batista <klose.eric31@gmail.com>
2021-08-05 17:05:01 -0300

    ADD: Create backtest calculations and plot (Issue: Drawdown)

--- src/backtest/backtest.py
+++ src/backtest/backtest.py
@@ -10,6 +10,8 @@ class Backtest(object):
     def __init__(self, setup: 'Setup()', dataframe: pd.DataFrame()) -> None:
         self.__setup = setup
 
+        self.__saldo = 100000
+
         if self.__setup is not None:
             self.__setup_dataframe = self.__setup.create_strategy(dataframe=dataframe)
         else: 
@@ -176,7 +178,11 @@ class Backtest(object):
     def processing_backtest_info_from_setup(self) -> None:
         self.__backtest_log_report_infos = pd.DataFrame.from_dict(self.__backtest_log_report_infos[-1]['position_closed'])
 
-        print(self.__backtest_log_report_infos)
+        self.__backtest_log_report_infos['saldo'] = self.__backtest_log_report_infos['result']
+
+        for i in range(len(self.__backtest_log_report_infos['result'])):
+            self.__saldo = self.__saldo + self.__backtest_log_report_infos['result'][i]
+            self.__backtest_log_report_infos['saldo'][i] = self.__saldo
 
         total_op = len(self.__backtest_log_report_infos)
 
@@ -195,6 +201,21 @@ class Backtest(object):
         lucro_bruto = round(sum(profit), 2)
         perda_bruta = round(sum(loss), 2)
 
+        perc_lucro = 100*qtd_profit/total_op
+        perc_loss = 100*qtd_loss/total_op
+
+        fator_lucro = abs(lucro_bruto/perda_bruta)
+
+        max_profit = max(profit)
+        max_loss = min(loss)
+
+        media_lucro = sum(profit)/len(profit)
+        media_perda = sum(loss)/len(loss)
+
+        razao = abs(round(media_lucro/media_perda, 2))
+
+        retorno = round((qtd_profit*media_lucro) + (qtd_loss*media_perda), 2)
+
         print(f'Total de operações: {total_op}')
         
         print(f'Total de Lucro Bruto: R$ {lucro_bruto}')
@@ -204,6 +225,69 @@ class Backtest(object):
         print(f'Total de trades prejudicial: {qtd_loss}')
 
         print(f'Lucro Liquido total: R$ {lucro_bruto + perda_bruta}')
+        
+        print(f'Percentual lucrativo: {round(perc_lucro, 2)}%')
+        print(f'Percentual prejudicial: {round(perc_loss, 2)}%')
+
+        print(f'Fator de Lucro: {fator_lucro}')
+
+        print(f'Ganho Máximo: R$ {round(max_profit, 2)}')
+        print(f'Perda Máxima: R$ {round(max_loss, 2)}')
+        
+        print(f'Média de ganhos: R$ {round(media_lucro, 2)}')
+        print(f'Média de perdas: R$ {round(media_perda, 2)}')
+
+        print(f'Razão ganho/perda: {razao}')
+        
+        print(f'Retorno: R$ {retorno}')
+
+        '''
+        >>> Gráficos auxiliares
+        '''
+        fig = plt.figure(figsize=(20, 10))
+
+        pizza = fig.add_subplot(2, 2, 1)
+        barra = fig.add_subplot(2, 2, 2)
+        hist = fig.add_subplot(2, 2, 3)
+        scatter = fig.add_subplot(2, 2, 4)
+
+        pizza.set_title('Percentual de Ganhos e Prejuízos')
+        barra.set_title('Média dos saldos positivos e negativos (R$)')
+        hist.set_title('Distribuição dos saldos (R$)')
+        scatter.set_title('Scatter dos saldos (R$)')
+
+        sizes = [qtd_profit, qtd_loss]
+        labels = ['Lucro', 'Perda']
+
+        pizza.pie(sizes, labels=labels, autopct='%1.2f%%', colors=['#33FF74', '#FF9933'])
+        
+        sns.barplot(data=self.__backtest_log_report_infos, x='in', y='result', palette=['#33FF74', '#FF9933'], ax=barra)
+
+        sns.distplot(a=self.__backtest_log_report_infos['result'], ax=hist)
+        
+        sns.scatterplot(data=self.__backtest_log_report_infos, x='start_date', y='result', ax=scatter)
+
+        plt.show()
+
+
+        self.__backtest_log_report_infos['saldo_max'] = self.__backtest_log_report_infos['saldo'].cummax()
+        self.__backtest_log_report_infos['saldo_min'] = self.__backtest_log_report_infos['saldo'].cummin()
+
+        self.__backtest_log_report_infos['saldo_ant'] = self.__backtest_log_report_infos['saldo_min'].shift(-1)
+
+        self.__backtest_log_report_infos['drawdowns_calc'] = (self.__backtest_log_report_infos['saldo_max'] - self.__backtest_log_report_infos['saldo_ant'])/self.__backtest_log_report_infos['saldo_max']*100
+
+        self.__backtest_log_report_infos['drawdowns'] = self.__backtest_log_report_infos['saldo'] - self.__backtest_log_report_infos['drawdowns_calc']*100
+
+        sns.lineplot(data=self.__backtest_log_report_infos, x='start_date', y='drawdowns', label='Drawdowns')
+        sns.lineplot(data=self.__backtest_log_report_infos, x='start_date', y='saldo', label='Saldo')
+
+        print(self.__backtest_log_report_infos.tail(5))
+
+        plt.legend()
+        plt.show()
+
+
         
 
     def export_backtest_log_report(self, results: dict) -> print:
